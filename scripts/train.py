0530fea0179ca02a4c8441b7974ce46ee90d1bc2
cagriefe <cagriefe34@gmail.com>
2025-01-15 14:57:36 +0300

    Refactor training script and train the model

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,7 @@
-# scripts/train.py
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -20,8 +23,8 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs):
     model.train()
     for epoch in range(num_epochs):
         for sequences, labels in train_loader:
-            sequences = sequences.to(device)
-            labels = labels.to(device)
+            sequences = sequences.float().to(device)
+            labels = labels.float().to(device)
             outputs = model(sequences)
             loss = criterion(outputs, labels)
             optimizer.zero_grad()
@@ -30,14 +33,17 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs):
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 if __name__ == "__main__":
-    with open('config.json') as f:
+    with open('scripts/config.json') as f:
         config = json.load(f)
 
     data = pd.read_csv('data/scaled_btc_usdt.csv').values
     seq_length = config['seq_length']
     sequences = create_sequences(data, seq_length)
-    sequences = np.array(sequences)
-    X, y = sequences[:, 0], sequences[:, 1]
+    
+    # Separate sequences and labels
+    X, y = zip(*sequences)
+    X, y = np.array(X), np.array(y)
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     train_loader = torch.utils.data.DataLoader(list(zip(X_train, y_train)), batch_size=config['batch_size'], shuffle=True)
     test_loader = torch.utils.data.DataLoader(list(zip(X_test, y_test)), batch_size=config['batch_size'], shuffle=False)
